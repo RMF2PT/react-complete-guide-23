4108204ccb679c62b06c3b469ee4dfcbcd4c2e60
rmf2pt <rmf2pt@gmail.com>
2023-02-23 18:44:09 +0000

    Add props to Expense

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import "./ExpenseItem.css";
 
-export default function ExpenseItem() {
-  const expenseDate = new Date().toLocaleDateString();
-  const expenseTitle = "Xbox Gamepass";
-  const expensePrice = 12.99;
-
+export default function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate}</div>
+      <div>{props.date.toLocaleDateString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expensePrice}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
